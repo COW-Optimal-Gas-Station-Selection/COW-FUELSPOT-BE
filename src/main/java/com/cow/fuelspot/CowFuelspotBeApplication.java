e27177f95237fbca966df2d207d31d0250e20304
사용자 이름 <dlwhdgur0509@gmail.com>
2026-01-08 21:39:48 +0900

    feat: 프로젝트 초기 환경 설정
    스프링 부트 프로젝트 기본 구조 생성 및 MySQL 8.4 DB 환경 구축
    application.yml 설정 업데이트
    
    [팀원 안내]
    프로젝트 pull 후, 터미널에서 아래 명령어를 사용하여 DB 서버를 실행할 것
    docker-compose up -d

---
+++ src/main/java/com/cow/fuelspot/CowFuelspotBeApplication.java
@@ -0,0 +1,13 @@
+package com.cow.fuelspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowFuelspotBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CowFuelspotBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cow/fuelspot/CowFuelspotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cow.fuelspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowFuelspotBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
