26a335c1b0305ab7872cc17672a13f4701e20903
사용자 이름 <dlwhdgur0509@gmail.com>
2026-01-16 03:05:42 +0900

    refactor: 오피넷 DTO 변수명 개선 및 XML 파싱 적용

--- src/main/java/com/cow/fuelspot/external/dto/OpinetAroundResponseDTO.java
+++ src/main/java/com/cow/fuelspot/external/dto/OpinetAroundResponseDTO.java
@@ -1,24 +1,43 @@
 package com.cow.fuelspot.external.dto;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
 @Getter
 @Setter
+@JacksonXmlRootElement(localName = "RESULT")
 public class OpinetAroundResponseDTO {
 
+    @JacksonXmlProperty(localName = "OIL")
+    @JacksonXmlElementWrapper(useWrapping = false)
     private List<GasStationDTO> RESULT;
 
     @Getter
     @Setter
     public static class GasStationDTO {
-        private String UNI_ID;
-        private String POLL_DIV_CD;
-        private String OS_NM;
-        private int PRICE;
-        private double DISTANCE;
-        private double GIS_X_COOR;
-        private double GIS_Y_COOR;
+        @JacksonXmlProperty(localName = "UNI_ID")
+        private String uniId;
+
+        @JacksonXmlProperty(localName = "POLL_DIV_CD")
+        private String pollDivCd;
+
+        @JacksonXmlProperty(localName = "OS_NM")
+        private String osNm;
+
+        @JacksonXmlProperty(localName = "PRICE")
+        private int price;
+
+        @JacksonXmlProperty(localName = "DISTANCE")
+        private double distance;
+
+        @JacksonXmlProperty(localName = "GIS_X_COOR")
+        private double gisXCoor;
+
+        @JacksonXmlProperty(localName = "GIS_Y_COOR")
+        private double gisYCoor;
     }
 }
