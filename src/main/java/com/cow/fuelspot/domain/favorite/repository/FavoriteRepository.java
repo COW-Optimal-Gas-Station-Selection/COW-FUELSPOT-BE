fb58425b2d32349defb04291383f830ab139e053
사용자 이름 <dlwhdgur0509@gmail.com>
2026-01-25 23:50:14 +0900

    Feat: 즐겨찾기(Favorite) Entity 및 Repository 설계

---
+++ src/main/java/com/cow/fuelspot/domain/favorite/repository/FavoriteRepository.java
@@ -0,0 +1,19 @@
+package com.cow.fuelspot.domain.favorite.repository;
+
+import com.cow.fuelspot.domain.favorite.entity.Favorite;
+import com.cow.fuelspot.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+
+    List<Favorite> findAllMember(Member member);
+
+    boolean existsByMemberAndStationId(Member member, String stationId);
+
+    void deleteByMemberAndStationId(Member member, String stationId);
+
+    long countByStationId(String stationId);
+}
