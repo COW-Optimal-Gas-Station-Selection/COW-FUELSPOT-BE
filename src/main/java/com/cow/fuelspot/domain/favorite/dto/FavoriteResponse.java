4497387b92eab38e6edf0e9dbaba690e0d0c05bd
사용자 이름 <dlwhdgur0509@gmail.com>
2026-01-26 01:36:00 +0900

    Feat: 즐겨찾기 Controller 구현 및 Swagger 예시값(Schema) 적용

--- src/main/java/com/cow/fuelspot/domain/favorite/dto/FavoriteResponse.java
+++ src/main/java/com/cow/fuelspot/domain/favorite/dto/FavoriteResponse.java
@@ -1,18 +1,23 @@
 package com.cow.fuelspot.domain.favorite.dto;
 
 import com.cow.fuelspot.domain.favorite.entity.Favorite;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class FavoriteResponse {
-    private  Long favoriteId;
-    private  String StationId;
+
+    @Schema(description = "즐겨찾기 고유 ID (PK)", example = "1")
+    private Long favoriteId;
+
+    @Schema(description = "오피넷 주유소 ID", example = "A0001234")
+    private String stationId;
 
     @Builder
     public FavoriteResponse(Long favoriteId, String stationId){
         this.favoriteId = favoriteId;
-        this.StationId = stationId;
+        this.stationId = stationId;
     }
 
     public  static  FavoriteResponse from(Favorite favorite){
--- src/main/java/com/cow/fuelspot/domain/favorite/service/FavoriteService.java
+++ src/main/java/com/cow/fuelspot/domain/favorite/service/FavoriteService.java
@@ -17,19 +17,16 @@ public class FavoriteService {
     private final FavoriteRepository favoriteRepository;
     private final MemberRepository memberRepository;
 
-    //즐겨찾기 추가
     @Transactional
     public FavoriteResponse addFavorite(String email, String stationId) {
-        // 회원 조회
+
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
 
-        // 중복 체크 (이미 찜했으면 에러)
         if (favoriteRepository.existsByMemberAndStationId(member, stationId)) {
             throw new IllegalStateException("이미 즐겨찾기 된 주유소입니다.");
         }
 
-        // 저장
         Favorite favorite = Favorite.builder()
                 .member(member)
                 .stationId(stationId)
@@ -39,17 +36,14 @@ public class FavoriteService {
 
         return FavoriteResponse.from(favorite);
     }
-    // 즐겨찾기 취소
+
     @Transactional
     public void removeFavorite(String email, String stationId) {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
-
-        // 삭제
         favoriteRepository.deleteByMemberAndStationId(member, stationId);
     }
 
-    // 해당 주유소를 찜한 사람 수 조회 (Count)
     public long getFavoriteCount(String stationId) {
         return favoriteRepository.countByStationId(stationId);
     }
