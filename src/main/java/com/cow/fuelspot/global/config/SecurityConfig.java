cf1af287eabcda4c2e7297e2c4d7a2ddf0996737
사용자 이름 <dlwhdgur0509@gmail.com>
2026-01-26 15:41:33 +0900

    refactor: SecurityConfig 컴파일 에러 수정 (throws Exception 추가)

--- src/main/java/com/cow/fuelspot/global/config/SecurityConfig.java
+++ src/main/java/com/cow/fuelspot/global/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 
     // 보안 필터 체인 설정
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
