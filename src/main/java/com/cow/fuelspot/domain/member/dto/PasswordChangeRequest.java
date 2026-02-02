26009e0a38a4c1a78f25e7564d3b99f942b24457
ejjun <jyj020516@gmail.com>
2026-02-02 21:50:38 +0900

    feat: 회원가입 및 비밀번호 변경 시 비밀번호 확인 검증 로직 추가

--- src/main/java/com/cow/fuelspot/domain/member/dto/PasswordChangeRequest.java
+++ src/main/java/com/cow/fuelspot/domain/member/dto/PasswordChangeRequest.java
@@ -16,4 +16,7 @@ public class PasswordChangeRequest {
     @NotBlank(message = "새 비밀번호를 입력해주세요.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     private String newPassword;
+
+    @NotBlank(message = "새 비밀번호를 다시 한번 입력해주세요.")
+    private String checkNewPassword;
 }
